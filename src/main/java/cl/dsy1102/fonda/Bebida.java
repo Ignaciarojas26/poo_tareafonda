70714ab8f08d64bd18ea3ca62583d97e0ce2ff12
Ignacia <ie.rojas@duocuc.cl>
2026-09-24 12:37:16 -0300

    listo

--- src/main/java/cl/dsy1102/fonda/Bebida.java
+++ src/main/java/cl/dsy1102/fonda/Bebida.java
@@ -4,11 +4,11 @@ public abstract class Bebida {
     private String nombre;
     private int volumenML;
     private int stock;
-
+//constructor
     public Bebida(String nombre, int volumenML, int stock) {
-        this.nombre=nombre;
-        this.volumenML=volumenML;
-        this.stock=stock;
+       setNombre(nombre);
+       setVolumenML(volumenML);
+       setStock(stock);
     }
 
     public String getNombre() {
@@ -16,25 +16,40 @@ public abstract class Bebida {
     }
 
     public void setNombre(String nombre) {
+        if (nombre == null || nombre.trim().isEmpty()) {
+            throw new IllegalArgumentException("El nombre no puede estar vacio");
+        }
         this.nombre = nombre;
     }
+
     public int getVolumenML() {
         return volumenML;
     }
+
     public void setVolumenML(int volumenML) {
+        if (volumenML < 100 || volumenML > 3000) {
+            throw new IllegalArgumentException("El volumen esta fuera de rango");
+        }
         this.volumenML = volumenML;
     }
     public int getStock() {
+
         return stock;
     }
     public void setStock(int stock) {
+        if (stock <= 0) {
+            throw new IllegalArgumentException("El stock debe ser mayor a 0");
+        }
         this.stock = stock;
     }
 
     public abstract double calcularPrecio();
     public abstract String obtenerDetalle();
 
-
+    @Override
+    public String toString() {
+        return nombre + "-" + volumenML + "ml";
+    }
 
 }
 
